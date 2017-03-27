34fcc965be1b90f420a9b29af9bcfd923de34acb
Jiawdft <jiawei@hotmail.fr>
2017-03-27 21:44:54 +0200

    update

--- tp3Graph/src/fr/isep/lab3/Main.java
+++ tp3Graph/src/fr/isep/lab3/Main.java
@@ -7,8 +7,8 @@ public class Main {
 	public static void main(String[] args) {
 
 			GraphV2 test = new GraphV2("graph.txt");
-		//	test.affichageAdj();
+			test.affichageAdj();
 			
-			test.addEdge(1, 2);
+	//		test.addEdge(1, 2);
 	}
 }
